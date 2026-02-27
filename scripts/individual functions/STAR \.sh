a28231ba5a9632993df42654260ead7b4a6e9cd8
jock231198-maker <jock231198@gmail.com>
2026-02-27 16:16:30 -0600

    Star.sh update did not work

--- scripts/individual functions/STAR \.sh
+++ scripts/individual functions/STAR \.sh
@@ -1,12 +1,10 @@
 STAR \
 --outSAMattributes All \
 --outSAMtype BAM SortedByCoordinate \
---quantMode GeneCounts \
 --runThreadN 8 \
 --outReadsUnmapped Fastx \
 --outMultimapperOrder Random \
 --outWigType wiggle \
---genomeDir STARresults/StarIndexAae \
---readFilesIn JKViromedata/M1_M11/M1_S1_R1.fastq JKViromedata/M1_M11/M1_S1_R2.fastq \
---outFileNamePrefix STARresults/STARalign \
---sjdbGTFfile Aae_refgenome/genomic.gtf
+--genomeDir /Users/JK/Desktop/STARresults/Star_Index \
+--readFilesIn /Users/JK/Desktop/JKViromedata/M2/M2_S2_L001_R1_001.fastq \
+--outFileNamePrefix /Users/JK/Desktop/STARresults/Star_Map/M1_reads 
