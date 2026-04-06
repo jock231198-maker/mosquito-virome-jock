7370f119e74ee9bba1f6bd5c5e1bfef4442b059f
jock231198-maker <jock231198@gmail.com>
2026-04-06 10:49:16 -0600

    update on bowtie_map.sh for pass the files to bam instantly

--- scripts/individual functions/bowtie_map.sh
+++ scripts/individual functions/bowtie_map.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #bowtiemap
+#runlike this ./bowtie_map.sh "/directory/to/my/viromedata/"
 input_dir="$1"
 find "$input_dir" -type f -name "*_R1_001.fastq.gz" | while read -r file; do
   sample=$(basename "$file" | cut -d'_' -f1-2)
@@ -10,7 +11,7 @@ find "$input_dir" -type f -name "*_R1_001.fastq.gz" | while read -r file; do
 read1="$input_dir/${sample}_R1_001.fastq.gz"
 read2="$input_dir/${sample}_R2_001.fastq.gz"
 if [[ -f "$read1" && -f "$read2" ]]; then
-bowtie2 -p 4 -x /Users/JK/Desktop/Bow/IndexBow/Aae_index/Aae_index -1 "$read1" -2 "$read2" -S /${outdir}/${sample}.sam
+bowtie2 -p 4 -x /Users/JK/Desktop/Bow/IndexBow/Aae_index/Aae_index -1 "$read1" -2 "$read2" -S /${outdir}/${sample}.sam | samtools view -bS - > ${outdir}/${sample}.bam
 else
 echo "Missing files for $sample R1 and $sample R2"
 fi
