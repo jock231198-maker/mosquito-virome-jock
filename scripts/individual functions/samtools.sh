e94966a6ec08e928137e1ac6bfc041b9e7965b2a
jock231198-maker <jock231198@gmail.com>
2026-05-07 11:50:46 -0600

    in samtools there is a spades line for allign

--- scripts/individual functions/samtools.sh
+++ scripts/individual functions/samtools.sh
@@ -4,3 +4,6 @@ samtools view -b -f 4 mapped.bam > unmapped.bam
 
 # Ejemplo de comando para convertir un archivo BAM a FASTQ usando samtools
 samtools fastq unmapped.bam > unmapped.fastq
+
+
+rnaviralspades.py --12 /Users/JK/Desktop/bowtie2/MapBow/trimm_cat_fastq/M1_S1.unmapped.fastq -o /Users/JK/Desktop/spades
